c8d6d1b08c49852303f6d6e54995a8735dfd9619
Iain Nicol <git@iainnicol.com>
2017-03-03 21:37:45 +0000

    Port to Rust
    Rust is a good language to write a standard library in.  Rust doesn't
    require much runtime support, and can import and export C compatible
    functions.  At the same time, Rust code can be quite high level.

---
+++ src/interaction.rs
@@ -0,0 +1,41 @@
+// Copyright © 2017 Iain Nicol
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+extern crate user32;
+extern crate winapi;
+
+use std::ffi::OsStr;
+use std::iter;
+use std::os::windows::ffi::OsStrExt;
+use std::ptr;
+
+fn encode_wide(s: &str) -> Vec<u16> {
+    OsStr::new(s).encode_wide().chain(iter::once(0)).collect()
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+// TODO: implement this function against the correct prototype.
+// Currently we implement only the first parameter, which we take as a
+// string.  Also, the return type is wrong.
+pub extern "system" fn MsgBox(prompt: *const u16) {
+    let project1 = encode_wide("Project1");
+    unsafe {
+        user32::MessageBoxW(ptr::null_mut(),
+                            prompt,
+                            project1.as_ptr(),
+                            winapi::MB_OK)
+    };
+}
