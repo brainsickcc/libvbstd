4de408f9bb4795e739b9132ed509629720803bed
Iain Nicol <git@iainnicol.com>
2017-06-17 19:24:47 +0100

    Tweak source code format
    Change the format specification.  Also, run the nightly version of
    rustfmt against the codebase.  Said newer version of rustfmt changes the
    indentation style.

--- src/interaction.rs
+++ src/interaction.rs
@@ -1,17 +1,18 @@
 // Copyright © 2017 Iain Nicol
 
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
 
 extern crate user32;
 extern crate winapi;
@@ -33,9 +34,11 @@ fn encode_wide(s: &str) -> Vec<u16> {
 pub extern "system" fn MsgBox(prompt: *const u16) {
     let project1 = encode_wide("Project1");
     unsafe {
-        user32::MessageBoxW(ptr::null_mut(),
-                            prompt,
-                            project1.as_ptr(),
-                            winapi::MB_OK)
+        user32::MessageBoxW(
+            ptr::null_mut(),
+            prompt,
+            project1.as_ptr(),
+            winapi::MB_OK,
+        )
     };
 }
